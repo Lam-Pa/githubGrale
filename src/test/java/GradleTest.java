25640d5d12a06a175163bc1fd31c3be5008b0351
Lam-Pa <reyvishnyakov@gmail.com>
2020-08-02 23:00:06 +0300

    first

--- src/test/java/GradleTest.java
+++ src/test/java/GradleTest.java
@@ -1,5 +1,6 @@
 public class GradleTest {
     public static void main(String[] args) {
         System.out.println("Hello Gradle from Google!!!");
+        System.out.println("hello world!");
     }
 }
